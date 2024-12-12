7c66151f5e571147a31eb843ed37124bc5637689
nathan.sineiro <nathan.sineiro@devnology.com.br>
2024-12-12 16:08:40 -0300

    :ok_hand: feat: Adicionado CopyWith

--- lib/src/model/task.dart
+++ lib/src/model/task.dart
@@ -1,6 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 
-class Task extends Equatable{
+class Task extends Equatable {
   final int id;
   final String description;
   final String check;
@@ -9,4 +10,16 @@ class Task extends Equatable{
   
   @override
   List<Object?> get props => [id, description, check];
+
+  Task copyWith({
+    int? id,
+    String? description,
+    String? check,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      description: description ?? this.description,
+      check: check ?? this.check,
+    );
+  }
 }
